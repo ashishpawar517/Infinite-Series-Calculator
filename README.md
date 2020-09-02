4c92be217c71545ca1b4cbebc4c3ab71197a927d
Aashish <pawarashish564@gmail.com>
2020-09-02 11:24:06 +0530

    🎉 Initial Commit: 1.0 and Readme

---
+++ README.md
@@ -0,0 +1,69 @@
+<div align="center">
+	<h1>Infinite Series Tracker </h1>
+    <br>
+
+[![forthebadge](https://forthebadge.com/images/badges/made-with-python.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/not-an-issue.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) 
+[![forthebadge](https://forthebadge.com/images/badges/check-it-out.svg)](https://forthebadge.com)
+
+<!-- $\sum_{i=1}^{10} t_i$ -->
+<!-- ![equation](https://latex.codecogs.com/svg.latex?\sum_{n=0}^{100000}%20f(n)) -->
+
+<table width='100%' align="center">
+    <tr align='center'>
+        <td align='center' width='100%' colspan='2'>
+           <strong>
+           <code>Simple Infinite Series Calculator</code></strong><br />
+            👉 Just Put a Series function,upper and lower limits.
+            <br>
+            <img src="https://latex.codecogs.com/svg.latex?\sum_{n=0}^{100000}%20f(n)">
+        </td>
+    </tr>
+    <tr align='center' >
+        <td align='center'>
+            A FOSS (Free & Open Source Software) project. Maintained by <a href='https://github.com/pawarashish564'>@Aashish Pawar ⚡</a>.
+        </td>
+    </tr>
+<tr align='center'  width='100%'><td align='center'><sup> Follow Ahmad's #FOSS work on GitHub <a href='https://github.com/Pawarashish564'>@Aashish Pawar⚡</a> 
+</a></sup></td><td align='center'>🙌</td></tr>
+</table>
+
+</div>
+
+<br>
+
+# 👉 `Test a Infinite Series Function`
+
+> `just use -f command line option with a function like 1/n , 1/n**2, 1/fact(n),1/n*2,1/1/n**3` etc and provide upper and lower limit (starting and ending points of a series).
+
+`Supported Mathematical functions` are:
+
+- 🥞 fact(n) ✓ (calculation many n! type of series)
+- 🤠 trigonometric functions sin(n), cos(n), tan(n) ✓
+- 🗃 More Functions coming soon ✓
+<!-- - 🐎 sqrt ✓ -->
+<!-- - 💯 other series functions ✓ -->
+<br>
+
+## GETTING STARTED!
+
+Let's check simple infinite series convergence..
+
+#### ⚡️ Quick Overview
+Consider this series 
+<br>
+<p align="center">
+<img src="https://latex.codecogs.com/svg.latex?\sum_{n=0}^{%20\infty%20}%201/n!">
+</p>
+we know that this series converge to Eular constant (e). let check that using the tool.
+<br>
+Run the following. 💯
+
+```sh
+python series_calc.py -f 1/fact(n) -l 0
+```
+
+
+> 🎛   _you'll get 2.718281828459045223732791526816949842789 which exacly equal to constant e_.
+
---
+++ series_calc.py
@@ -0,0 +1,104 @@
+
+# for math functionalities
+import math as m
+# for high precision float handling
+from decimal import *
+# for argument parsing command line
+import argparse
+# for docstrings and text dedent
+import textwrap
+# import time
+# color codes
+CRED = '\033[91m'
+CGREEN= '\033[92m'
+CYELLOW='\033[33m'
+CEND = '\033[0m'
+# Basic Math function definitions
+def log(n):
+    return m.log10(n)
+
+def ln(n):
+    return m.log(n)
+
+def fact(n):
+    return m.factorial(n)
+
+def sin(n):
+    return m.sin(n)
+
+def cos(n):
+    return m.cos(n)
+
+def tan(n):
+    return m.tan(n)
+
+
+# command line argument processing
+parser = argparse.ArgumentParser(
+    prog='series_calc', formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=textwrap.dedent('''\
+         Current supported Math functions in Infinite series!
+         ---------------------------------------------------
+            1. log(n) = log of n to the base 10
+            2. ln(n) = log of n to the base e
+            3. fact(n) = factorial of n or n!
+            4. sin(n) = trigonometric sin(x)
+            5. cos(n) = trigonometric cos(x)
+            6. tan(n) = trigonometric tan(x)
+         '''))
+parser.add_argument('-u ', '--upper', type=int,
+                    default=500, help='max # no of terms')
+parser.add_argument('-l ', '--lower', type=int, default=1,
+                    help='min term to start with infinite series ')
+parser.add_argument(
+    '-f', nargs='*', help='function for infinite series (function should be in terms of n)', required=True)
+parser.add_argument('-s', type=int, default=32, help='set new precision')
+parser.add_argument('-v', '--version', action='version',
+                    version='series_calc.py - 1.0')
+args = parser.parse_args()
+
+# first argument
+# print(args.f[0])
+try:
+# create a lambda function using eval
+    fn = eval('lambda n:' + args.f[0])
+# fn = eval(fn)
+
+# set new precision 
+    getcontext().prec = args.s
+
+# infinite series
+# as this is going to be a finite series but as the principle hypothesis in infinite series states that
+# any convergent infinite series will converge to some arbitrary constant after some iteration.
+    res = Decimal(0.0)
+
+    upper_limit = args.upper
+    lower_limit = args.lower
+    for i in range(lower_limit, upper_limit+1):
+        res = res + Decimal(fn(i))
+        if i%6 == 1:
+            print(CYELLOW,"[._____] : =>", res,CEND,end='\r')
+        elif i%6 == 2:
+            print(CYELLOW,"[__.___] : =>", res,CEND,end='\r')
+        elif i%6 == 3:
+            print(CYELLOW,"[___.__] : =>", res,CEND,end='\r')
+        elif i%6 == 4:
+            print(CYELLOW,"[____._] : =>", res,CEND,end='\r')
+        elif i%6 == 5:
+            print(CYELLOW,"[_____.] : =>", res,CEND,end='\r')
+        else:
+            print(CYELLOW,"[______] : =>", res,CEND,end='\r')
+        
+        # time.sleep(1.0)
+    
+    # printing the result
+    print(CGREEN,"[OUTPUT] : =>", res,CEND,end="\r")
+
+except ZeroDivisionError as identifier:
+    print(CRED,'[ERROR] Please change the lower limit. (start with 1)',CEND)
+
+except SyntaxError as identifier:
+    print(CRED,'[ERROR] Please check the function provided. (missing brackets or incomplete/incompatible operation)',CEND)
+
+except OverflowError as identifier:
+    print(CRED,'[ERROR] Please check the function provided. (Max limit Reached) last value found =',res,CEND)
