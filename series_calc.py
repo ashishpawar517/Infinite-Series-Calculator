1f0cebc04512323093c912a25e4ad20bfbb8b7b7
Aashish <pawarashish564@gmail.com>
2020-09-10 10:50:09 +0530

    🐛 FIX: using mpmath effciently for  calculation

--- series_calc.py
+++ series_calc.py
@@ -5,6 +5,7 @@ import argparse
 import textwrap
 # import mpmath lib
 from mpmath import * 
+import sys
 # color codes
 CRED = '\033[91m'
 CGREEN= '\033[92m'
@@ -19,20 +20,20 @@ parser = argparse.ArgumentParser(
          ---------------------------------------------------
             1. log(n) = log of n to the base 10
             2. ln(n) = log of n to the base e
-            3. fact(n) = factorial of n or n!
+            3. fac(n) = factorial of n or n!
             4. sin(n) = trigonometric sin(x)
             5. cos(n) = trigonometric cos(x)
             6. tan(n) = trigonometric tan(x)
             7. sqrt(n) = square root of a number
-            8. cuberoot(n) = cuberoot of a number
+            8. cbrt(n) = cuberoot of a number
          '''))
-parser.add_argument('-u ', '--upper', type=int,
-                    default=500, help='max # no of terms')
+parser.add_argument('-u ', '--upper',
+                    default='inf', help='max # no of terms')
 parser.add_argument('-l ', '--lower', type=int, default=1,
-                    help='min term to start with infinite series ')
+                    help='min term to start with infinite series')
 parser.add_argument(
     '-f', nargs='*', help='function for infinite series (function should be in terms of n)', required=True)
-parser.add_argument('-s', type=int, default=32, help='set new precision')
+parser.add_argument('-p', type=int, default=50, help='set new precision')
 parser.add_argument('-v', '--version', action='version',
                     version='series_calc.py - 1.0')
 args = parser.parse_args()
@@ -40,19 +41,29 @@ args = parser.parse_args()
 # first argument
 # print(args.f[0])
 try:
-    
-    mp.dps =args.s 
-    fn = eval(args.f[0])
-    
+    # efficiently
+    mp.dps =args.p
+    lower_limit = args.lower
+    upper_limit = args.upper
+    fn = None 
+    # try:
+    str_cmd = "nsum(lambda n:"+args.f[0]+",["+str(lower_limit)+","+str(upper_limit)+"])"
+    print(str_cmd)
+    fn = eval(str_cmd)
+        # print(fn)
+    # except(SyntaxError, NameError, TypeError, ZeroDivisionError):
+        # print(CRED,'[ERROR] Please check input provided in -f ',res,CEND)
+        # sys.exit(0)
+        
     res = fn
     #print result 
     print(CGREEN,"[OUTPUT] : =>", res,CEND,end="\r")
 
-except ZeroDivisionError :
-    print(CRED,'[ERROR] Please change the lower limit. (start with 1)',CEND)
-
+except ZeroDivisionError as e :
+    print(CRED,'[ERROR] Check the lower limit provided (Please change it to 1) ',CEND)
+# 
 except SyntaxError :
     print(CRED,'[ERROR] Please check the function provided. (missing brackets or incomplete/incompatible operation)',CEND)
 
-except NameError :
-    print(CRED,'[ERROR] Please check the function provided. (Max limit Reached) last value found =',res,CEND)
+except  NameError:
+    print(CRED,'[ERROR] Please check the function provided. (incompatible function .see help)',CEND)
